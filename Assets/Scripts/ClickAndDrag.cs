f1eb54a1d82863d1a5eff17e8e7aefb2ff2d394c
Jen <bourkj@rpi.edu>
2020-07-16 13:51:12 -0400

    Add toe functionality

---
+++ Assets/Scripts/ClickAndDrag.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDrag : MonoBehaviour
+{
+    [SerializeField] private Transform _canSpot;
+    [SerializeField] private Sprite _normalSprite = null;
+    [SerializeField] private Sprite _pourSprite = null;
+    
+    private bool _isClick = false;
+    private Rigidbody2D _can = null;
+    private SpriteRenderer _spriteRenderer = null;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _can = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        _isClick = true;
+    }
+
+    void OnMouseDrag()
+    {
+        if (_isClick == true){
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            _can.position = cursorPosition;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        _isClick = false;
+        _can.position = _canSpot.position;
+    }
+
+    void OnTriggerEnter2D()
+    {
+        _spriteRenderer.sprite = _pourSprite;
+    }
+
+    void OnTriggerExit2D()
+    {
+        _spriteRenderer.sprite = _normalSprite;
+    }
+}
---
+++ Assets/Scripts/Toes.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toes : MonoBehaviour
+{
+    [SerializeField] private Manager _manager = null;
+    //variables for clicking
+    [SerializeField] private Camera _camera = null;
+
+    private Animator _animator = null;
+    private Collider2D _collider = null;
+
+
+    //variables for spawner
+    [SerializeField] private Transform _spawnPoint = null;
+    [SerializeField] private GameObject _toe = null;
+    
+
+
+
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if you click on the toe, it will trigger an animation of being pulled out of the dirt
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D plant = Physics2D.OverlapPoint(mousePosition);
+            if(plant == _collider && _animator.GetCurrentAnimatorStateInfo(0).IsName("ToePulled") != true)
+            {
+                _animator.SetTrigger("pulled");
+                _manager.toeNumber++;
+                print(_manager.toeNumber);
+                ToeSpawn();
+            }
+        }
+    }
+
+    void ToeSpawn()
+    {
+        Instantiate(_toe, _spawnPoint.position, Quaternion.identity);
+    }
+}
