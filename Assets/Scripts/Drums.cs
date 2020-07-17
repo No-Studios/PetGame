05651b34148491c4d0295bdf02bf2ddf76ecf85a
Leonardo Price <leonardo.price@gmail.com>
2020-07-16 23:02:31 -0400

    drum and eat sounds implemented

--- Assets/Scripts/Drums.cs
+++ Assets/Scripts/Drums.cs
@@ -9,26 +9,51 @@ public class Drums : MonoBehaviour
 
     [HideInInspector] public float _drumStartTime = 10f;
     [HideInInspector] public float _drumTimeLeft = 10f;
+    private AudioSource audioSource;
+    private bool druming = false;
+    private float timeChange = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         _collider = GetComponent<Collider2D>();
         _drumTimeLeft = _drumStartTime;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        _drumTimeLeft -= Time.deltaTime;
+        _drumTimeLeft -= Time.deltaTime *timeChange;
+        Debug.Log(_drumTimeLeft);
 
         if(Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
 			Collider2D plant = Physics2D.OverlapPoint(mousePosition);
-            if(plant == _collider)
+            if(plant == _collider && druming == false)
             {
-                _drumTimeLeft = _drumStartTime;
+                druming = true;
+                
+                audioSource.Play();
+                timeChange = -1;
             }
+            else if (plant == _collider && druming == true)
+            {
+                druming = false;
+                timeChange = 1;
+                audioSource.Stop();
+            }
+            
         }
+        if (_drumTimeLeft >= _drumStartTime)
+        {
+            Debug.Log("weee");
+            _drumTimeLeft = _drumStartTime;
+            druming = false;
+            timeChange = 1;
+            audioSource.Stop();
+        }
+
     }
 }
--- Assets/Scripts/ToeDestroyer.cs
+++ Assets/Scripts/ToeDestroyer.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class ToeDestroyer : MonoBehaviour
 {
     [SerializeField] private Toes _toes = null;
+    [SerializeField]
+    private AudioClip[] clips = null;
+    public AudioSource audioSource = null;
+
+
+    private void Start()
+    {
+        audioSource = audioSource.GetComponent<AudioSource>();
+       
+    }
+
     void OnTriggerStay(Collider2D collider)
     {
         if(_toes.eating == true && collider.tag == "ToeDestroyer")
         {
+            AudioClip clip = GetRandomClip();
+            audioSource.PlayOneShot(clip);
             Destroy(gameObject);
         }
 
+    }
+    private AudioClip GetRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
     }
 }
