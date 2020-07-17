6f3ee74fe960dc1c2c1df22ce27c119aaa8776a7
Jen <bourkj@rpi.edu>
2020-07-16 20:37:43 -0400

    Add drums

---
+++ Assets/Scripts/Drums.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drums : MonoBehaviour
+{
+    [SerializeField] private Camera _camera = null;
+    private Collider2D _collider = null;
+
+    [HideInInspector] public float _drumStartTime = 10f;
+    [HideInInspector] public float _drumTimeLeft = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _collider = GetComponent<Collider2D>();
+        _drumTimeLeft = _drumStartTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _drumTimeLeft -= Time.deltaTime;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D plant = Physics2D.OverlapPoint(mousePosition);
+            if(plant == _collider)
+            {
+                _drumTimeLeft = _drumStartTime;
+            }
+        }
+    }
+}
--- Assets/Scripts/NeedTimer.cs
+++ Assets/Scripts/NeedTimer.cs
@@ -8,6 +8,7 @@ public class NeedTimer : MonoBehaviour
     [SerializeField] private bool _thirst = false;
     [SerializeField] private bool _hunger = false;
     [SerializeField] private bool _fun = false;
+    [SerializeField] private Drums _drums = null;
     private Transform _timer;
 
     private float _initialScale;
@@ -30,7 +31,10 @@ public class NeedTimer : MonoBehaviour
             _timeLeft = _pet.current_hungry_time;
         } else if(_thirst == true && _hunger == false && _fun == false){
             _timeLeft = _pet.current_thirsty_time;
+        } else if(_thirst == false && _hunger == false && _fun == true){
+            _timeLeft = _drums._drumTimeLeft;
         }
+        print(_drums._drumTimeLeft);
         //_timeLeft -= Time.deltaTime;
         //print(_pet.current_hungry_time);
         //print(_initialTime);
